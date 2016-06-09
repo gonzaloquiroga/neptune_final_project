fca2f316e03ba8302cd4d0bd795940f399f0b6a0
Gonzalo <maguszzz@hotmail.com>
2016-06-09 18:51:39 +0200

    Scrip working!

--- combinedsearch.py
+++ combinedsearch.py
@@ -17,30 +17,31 @@ OutFileD = open (OutFileName_Other, 'w')
 
 for Line in InFile:
 	
+	
 	if 'rhodopsin' in Line or 'Rhodopsin' in Line:
 		
-		Line = Line + "Class A \n"
+		#Line = Line + "Class A \n"
 		print (Line)
 		
 		OutFileA.write (Line)
 
 	elif 'Secretin' in Line or 'secretin' in Line:
 		
-		Line = Line + "Class B  \n"
+		#Line = Line + "Class B  \n"
 		print (Line)
 
 		OutFileB.write (Line)
 
 	elif 'Metabotropic' in Line or 'metabotropic' in Line:
 		
-		Line = Line + "Class C \n"
+		#Line = Line + "Class C \n"
 		print (Line)
 
 		OutFileC.write (Line)
 
 	elif 'Metabotropic' not in Line and 'rhodopsin' not in Line and 'Secretin' not in Line and 'metabotropic' not in Line and 'secretin' not in Line and 'Rhodopsin' not in Line: 
 
-		Line = Line + "Class Other \n"
+		#Line = Line + "Class Other \n"
 		print (Line)
 
 		OutFileD.write (Line)
--- match_contig_protein.py
+++ match_contig_protein.py
@@ -10,6 +10,7 @@ InFile2 = open (InFileName2, 'r' )
 OutFileName_ClassA = "ClassA_fasta.fas"
 OutFileA = open (OutFileName_ClassA, 'a')
 
+'''
 def Contig (ContigString1 , ContigString2):	
 
 	StringClassA = '(>\w+)(\t.*\n.+)'
@@ -22,18 +23,42 @@ def Contig (ContigString1 , ContigString2):
 	contig_name761 = Result761.group (1)
 	protein_seq = Result761.group (2)
 
+'''
+
+def Header_set (file):
+	H_set = set()
+
+	StringClassA = '(>\w+)(\t.*)'
+
+	for Line in file:
+
+		ResultA	= re.search( StringClassA, Line )
 
 
+		contig_nameA = ResultA.group (1)
 
-for Line in InFile2:
-	Line = Line.strip()
+		H_set.add (contig_nameA)
 
-	if 
+	return H_set
+
+H_set = Header_set(InFile)
+
+#print (H_set)
+
+String761 = '(>\w+)\n(.+)'
+Result761 = re.findall( String761, InFile2.read() )
+
+for result in Result761:
+
+	contig_name761 = result [0]
+	protein_seq = result [1]
+
+
+	if contig_name761 in H_set:
 		
-		Line = Line + "Class A \n"
-		print (Line)
+		print (contig_name761 + '\n' + protein_seq)
 		
-		OutFileA.write (Line)
+		OutFileA.write (contig_name761 + '\n' + protein_seq + '\n')
 
 	
 
